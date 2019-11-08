8e5ed363fba6f4d9e5677b83127e6f4d38ee6d38
Michael Gilbert <mj.gilbert@icloud.com>
2019-11-08 11:25:26 -0800

    initial push to github

---
+++ quotes_spider/quotes_spider/spiders/quotes.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class QuotesSpider(scrapy.Spider):
+    name = 'quotes'
+    allowed_domains = ['quotes.toscrape.com']
+    start_urls = ['http://quotes.toscrape.com/']
+
+    # Default callback method in spiders
+    def parse(self, response):
+        quotes = response.xpath('//*[@class="quote"]')
+        for quote in quotes:
+            text = quote.xpath('.//*[@class="text"]/text()').extract_first()
+            author = quote.xpath('.//*[@class="author"]/text()').extract_first()
+            tags = quote.xpath('.//*[@class="keywords"]/@content').extract_first()
+            
+            yield{'Text': text,
+                  'Author': author,
+                  'Tags': tags}
+
+        # Crawling pages
+        next_page_url = response.xpath('//*[@class="next"]/a/@href').extract_first()
+        # This line joins together the url we are crawling, and the next page url reference
+        absolute_next_page_url = response.urljoin(next_page_url)
+        yield scrapy.Request(absolute_next_page_url)  # If this wasn't in the parse function, we would need a callback function
